2ab365d6adcbcf5601984359f2d1b92a4af81f07
Rafael Santos de Araujo <rafaelsantosdearaujo@gmail.com>
2022-10-17 15:34:02 -0300

    adicionando configuracao de banco de dados

--- crud/src/main/java/br/com/psringboot/crud/Application.java
+++ crud/src/main/java/br/com/psringboot/crud/Application.java
@@ -2,6 +2,11 @@ package br.com.psringboot.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  *
@@ -11,5 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET )
+    @ResponseStatus(HttpStatus.OK)
+    public String metodoRetornaOlamundo(@PathVariable String nome) {
+    	return "Ola mundao " + nome;
+     	
     }
 }
